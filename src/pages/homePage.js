7e524caefe311163658d42ab0b7108116672d8b9
su199992 <ttw147@gmail.com>
2024-04-17 21:21:46 +0900

    사이드 메뉴 수정

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
-import React from 'react';
-
-const HomePage = () => {
-    return(
-        <div>
-            안녕하세요.
-        </div>
-    );
-};
-
-export default HomePage;
-=======
 import React, { useState } from 'react';
 import { Box, AppBar, Toolbar, IconButton, Typography, Menu, MenuItem, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-import InboxIcon from '@mui/icons-material/Inbox'; // 예제를 위해 추가
+import InboxIcon from '@mui/icons-material/Inbox';
 
 const HomePage = () => {
   const [anchorEl, setAnchorEl] = useState(null);
   const isMenuOpen = Boolean(anchorEl);
-  const [drawerOpen, setDrawerOpen] = useState(false); // 사이드바 열림 상태
+  const [drawerOpen, setDrawerOpen] = useState(true); // 사이드바 열림 상태를 true로 초기화
 
   const handleProfileMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
@@ -35,15 +22,22 @@ const HomePage = () => {
     handleMenuClose();
   };
 
-  const handleDrawerOpen = () => {
+  const toggleDrawer = () => {
     setDrawerOpen(!drawerOpen); // 사이드바 열기/닫기 토글
   };
 
   const drawer = (
     <Drawer
-      open={!drawerOpen}
-      onClose={handleDrawerOpen}
-      variant="temporary"
+      open={drawerOpen}
+      onClose={toggleDrawer}
+      variant="persistent"
+      sx={{
+        width: 240, 
+        flexShrink: 0,
+        top: '64px',
+        height: 'calc(100vh - 80px)', // 전체 높이에서 앱바 높이를 뺀 만큼 설정
+        '& .MuiDrawer-paper': { width: 240, top: '80px', height: 'calc(100vh - 64px)' } // 여기도 동일하게 적용
+      }}
     >
       <List>
         {['Upload', 'Starred', 'Send email'].map((text, index) => (
@@ -64,14 +58,15 @@ const HomePage = () => {
     <Box sx={{ display: 'flex' }}>
       <AppBar position="fixed">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            MyLogo
+          <Typography variant="h6" component="div" sx={{ fontSize: '50px'}}>
+            hunet
           </Typography>
           <IconButton
             size="large"
             color="inherit"
             aria-label="open drawer"
-            onClick={handleDrawerOpen} // 사이드바 열기/닫기 이벤트
+            onClick={toggleDrawer} // 사이드바 열기/닫기 이벤트
+            sx={{ marginLeft: 6}}  // 'hunet' 오른쪽 30px에 위치
           >
             <MenuIcon />
           </IconButton>
@@ -83,6 +78,7 @@ const HomePage = () => {
             aria-haspopup="true"
             onClick={handleProfileMenuOpen}
             color="inherit"
+            sx={{ marginLeft: 'auto' }}  // 가장 오른쪽에 위치
           >
             <AccountCircle />
           </IconButton>
@@ -116,4 +112,3 @@ const HomePage = () => {
 };
 
 export default HomePage;
->>>>>>> b268604 (homePage 메뉴바  추가)
